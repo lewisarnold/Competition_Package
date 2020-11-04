f8ce25c69874d7e081a52d940c8e2597fa9050f6
lewisarnold <lewis.arnold@shaw.ca>
2020-11-04 15:34:36 -0800

    added training controller

---
+++ training_data_controller/nodes/keyboard_control.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import threading
+
+import roslib; roslib.load_manifest('teleop_twist_keyboard')
+import rospy
+
+from geometry_msgs.msg import Twist
+
+import sys, select, termios, tty
+
+import time
+
+'''
+Inspired by teleop twist keyboard
+Reads from the keyboard and publishs movement commands to Twist
+'''
+
+initial_speed = 1
+initial_turn = 0.5
+
+start_msg = """
+Uses WASD and surrounding keys
+q   w   e
+a   s   d
+z   x   c
+
+w: toggle forward motion at set speed
+s: resets angular speed
+a: increments angular speed clockwise
+d: increments angular speed counterclockwise
+x: stops all movement
+e: increases increment size
+c: decreases increment size
+q: increases forward speed by 1
+z: decreases forward speed by 1
+
+initial forward speed = 10
+maximum angular speed = 5
+initial increment size = 5
+"""
+
+class PublishThread(threading.Thread):
+	def __init__(self, rate):
+		super(PublishThread, self).__init__()
+		self.publisher = rospy.Publisher('R1/cmd_vel', Twist, queue_size=1)
+		self.x = 0.0
+		self.y = 0.0
+		self.z = 0.0
+		self.th = 0.0
+		self.speed = 0.0
+		self.turn = 0.0
+		self.condition = threading.Condition()
+		self.done = False
+
+		# Set timeout to None if rate is 0 (causes new_message to wait forever
+		# for new data to publish)
+		if rate != 0.0:
+			self.timeout = 1.0 / rate
+		else:
+			self.timeout = None
+
+		self.start()
+
+	#wait till publisher is connected
+	def wait_for_subscribers(self):
+		i = 0
+		while not rospy.is_shutdown() and self.publisher.get_num_connections() == 0:
+			if i == 4:
+				print("Waiting for subscriber to connect to {}".format(self.publisher.name))
+			rospy.sleep(0.5)
+			i += 1
+			i = i % 5
+		if rospy.is_shutdown():
+			raise Exception("Got shutdown request before subscribers connected")
+
+	#Read from keyboard
+	def getKey(key_timeout):
+		tty.setraw(sys.stdin.fileno())
+		rlist, _, _ = select.select([sys.stdin], [], [], key_timeout)
+		if rlist:
+			key = sys.stdin.read(1)
+		else:
+			key = ''
+		termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+		return key
+
+	#process key 
+	def run(self):
+		twist = Twist()
+		while not self.done:
+			self.condition.acquire()
+			# Wait for a new message or timeout.
+			self.condition.wait(self.timeout)
+
+			# Copy state into twist message.
+			twist.linear.x = self.x * self.speed
+			twist.linear.y = self.y * self.speed
+			twist.linear.z = self.z * self.speed
+			twist.angular.x = 0
+			twist.angular.y = 0
+			twist.angular.z = self.th * self.turn
+
+			self.condition.release()
+
+			# Publish.
+			self.publisher.publish(twist)
+
+		# Publish stop message when thread exits.
+		twist.linear.x = 0
+		twist.linear.y = 0
+		twist.linear.z = 0
+		twist.angular.x = 0
+		twist.angular.y = 0
+		twist.angular.z = 0
+		self.publisher.publish(twist)
+
+
+	#update topic
+	def update(self, x, y, z, th, speed, turn):
+		self.condition.acquire()
+		self.x = x
+		self.y = y
+		self.z = z
+		self.th = th
+		self.speed = speed
+		self.turn = turn
+		# Notify publish thread that we have a new message
+		self.condition.notify()
+		self.condition.release()
+
+#prints the current forward and angular speed
+def vels(speed, turn):
+	return "currently:\tspeed %s\tturn %s " % (speed,turn)
+
+key_timeout = rospy.get_param("~key_timeout", 0.0)
+
+if __name__=="__main__":
+	settings = termios.tcgetattr(sys.stdin)
+
+	rospy.init_node('keyboard_controller')
+
+	speed = rospy.get_param("~speed", initial_speed)
+	turn = rospy.get_param("~turn", initial_turn)
+	repeat = rospy.get_param("~repeat_rate", 0.0)
+	key_timeout = rospy.get_param("~key_timeout", 0.0)
+	if key_timeout == 0.0:
+		key_timeout = None
+
+	pub_thread = PublishThread(repeat)
+
+	x = 0
+	y = 0
+	z = 0
+	th = 0
+	status = 0
+	try:
+		pub_thread.wait_for_subscribers()
+		pub_thread.update(x, y, z, th, speed, turn)
+
+		print(start_msg)
+		print(vels(speed,turn))
+        #time.sleep(5)
+
+		'''
+        while(1):
+            key = getKey(key_timeout)
+            if key in moveBindings.keys():
+                x = moveBindings[key][0]
+                y = moveBindings[key][1]
+                z = moveBindings[key][2]
+                th = moveBindings[key][3]
+            elif key in speedBindings.keys():
+                speed = speed * speedBindings[key][0]
+                turn = turn * speedBindings[key][1]
+
+                print(vels(speed,turn))
+                if (status == 14):
+                    print(msg)
+                status = (status + 1) % 15
+            else:
+                # Skip updating cmd_vel if key timeout and robot already
+                # stopped.
+                if key == '' and x == 0 and y == 0 and z == 0 and th == 0:
+                    continue
+                x = 0
+                y = 0
+                z = 0
+                th = 0
+                if (key == '\x03'):
+                    break
+ 
+            pub_thread.update(x, y, z, th, speed, turn)
+		'''
+	except Exception as e:
+		print(e)
