e85ef3f14b37f690bc64f9211a007da63d18cd0f
lewisarnold <lewis.arnold@shaw.ca>
2020-11-05 21:36:20 -0800

    improved speed of PID controller

--- pid_controller/nodes/pid_driver.py
+++ pid_controller/nodes/pid_driver.py
@@ -7,7 +7,7 @@ import numpy as np
 import cv2
 from cv_bridge import CvBridge
 
-FULL_SPEED_LINEAR = 0.15
+FULL_SPEED_LINEAR = 0.6
 
 ANGULAR_PROPORTIONAL = 0.15
 
@@ -19,8 +19,8 @@ ROAD_COLOUR_UPPER_BOUND = (90, 90, 90)
 DRIVE_VISION_STRIP = (450, 500)
 STRIP_CROPPING_X = 500
 
-NO_READING_ANGULAR_VELOCITY = 1
-NO_READING_LINEAR_DECREASE = 0.05
+NO_READING_LINEAR_DECREASE = 0.5
+NO_READING_ANGULAR_VELOCITY = 20 * FULL_SPEED_LINEAR * NO_READING_LINEAR_DECREASE
 
 
 class RobotDriver():
@@ -59,20 +59,21 @@ class RobotDriver():
         centre_x, center_found = self.find_road_center(road_detection_mask)
         centre_setpoint = int(cv_raw.shape[1] / 2)
 
-        # if not center_found:
-        #     if self.last_side_left:
-        #         velocity_command.angular.z = NO_READING_ANGULAR_VELOCITY
-        #     else:
-        #         velocity_command.angular.z = -NO_READING_ANGULAR_VELOCITY
-        #
-        #     velocity_command.linear.x = FULL_SPEED_LINEAR * NO_READING_LINEAR_DECREASE
+        if not center_found:
+            if self.last_side_left:
+                velocity_command.angular.z = NO_READING_ANGULAR_VELOCITY
+            else:
+                velocity_command.angular.z = -NO_READING_ANGULAR_VELOCITY
 
-        velocity_command.linear.x = FULL_SPEED_LINEAR
-        velocity_command.angular.z = FULL_SPEED_LINEAR * ANGULAR_PROPORTIONAL * (centre_setpoint - centre_x)
-        if centre_x > centre_setpoint:
-            self.last_side_left = False
+            velocity_command.linear.x = FULL_SPEED_LINEAR * NO_READING_LINEAR_DECREASE
+
         else:
-            self.last_side_left = True
+            velocity_command.linear.x = FULL_SPEED_LINEAR
+            velocity_command.angular.z = FULL_SPEED_LINEAR * ANGULAR_PROPORTIONAL * (centre_setpoint - centre_x)
+            if centre_x > centre_setpoint:
+                self.last_side_left = False
+            else:
+                self.last_side_left = True
 
         self.velocity_command_publisher.publish(velocity_command)
 
@@ -81,7 +82,7 @@ class RobotDriver():
         marked = cv2.circle(marked, (centre_setpoint, int((DRIVE_VISION_STRIP[0] + DRIVE_VISION_STRIP[1]) / 2)), 20,
                             (150, 250, 250), 20)
         cv2.imshow("marked", marked)
-        cv2.waitKey(10)
+        cv2.waitKey(1)
 
 
     def find_road_center(self, road_mask):
@@ -96,24 +97,24 @@ class RobotDriver():
             column_average[k] = np.average(cropped_strip[:, k])
 
         left = 0
-
+        set_left = False
         for j in range(width):
             if column_average[j] > AVERAGING_THRESHOLD:
                 left = j
+                set_left = True
                 break
 
         right = width - 1
+        set_right = False
         for j in reversed(range(left, width)):
             if column_average[j] > AVERAGING_THRESHOLD:
                 right = j
+                set_right = True
                 break
 
         centre_x = int((left + right) / 2)
-
-        # if left == 0 and right == width - 1:
-        #     return -1, False
-        # else:
-        return centre_x + STRIP_CROPPING_X, True
+        successfully_found_center = set_left and set_right
+        return centre_x + STRIP_CROPPING_X, successfully_found_center
 
         # marked = cv2.circle(road_mask, (centre, int((DRIVE_VISION_STRIP[0] + DRIVE_VISION_STRIP[1]) / 2)), 20, (150, 250, 250))
         # cv2.imshow("marked", marked)
